7879e4d1965517217f47fcb4b2c8d00dc615f982
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2025-02-28 02:00:04 +0300

    Exam prep.

--- Ex06_Barcode_Generator.py
+++ Ex06_Barcode_Generator.py
@@ -1,16 +1,9 @@
-start = int(input())
-end = int(input())
+start = input()
+end = input()
 
-for d1 in range(start, end + 1):
-    first = d1 // 1000
-    if first % 2 != 0:
-        for d2 in range(start, end + 1):
-            second = d2 // 100
-            if second % 2 != 0:
-                for d3 in range(start, end + 1):
-                    third = d3 // 10
-                    if third % 2 != 0:
-                        for d4 in range(start, end + 1):
-                            forth = d4
-                            if forth % 2 != 0:
-                                print(f'{forth}', end=' ')
+for a in range(int(start[0]), int(end[0]) + 1):
+    for b in range(int(start[1]), int(end[1]) + 1):
+        for c in range(int(start[2]), int(end[2]) + 1):
+            for d in range(int(start[3]), int(end[3]) + 1):
+                if a % 2 != 0 and b % 2 != 0 and c % 2 != 0 and d % 2 != 0:
+                    print(f"{a}{b}{c}{d}", end=" ")
